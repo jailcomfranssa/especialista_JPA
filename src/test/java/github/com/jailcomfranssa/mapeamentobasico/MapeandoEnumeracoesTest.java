53f655d37c4245a5a7ec9879ec3f5318a66be35b
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 20:53:10 -0300

    criando caso de teste cliente

---
+++ src/test/java/github/com/jailcomfranssa/mapeamentobasico/MapeandoEnumeracoesTest.java
@@ -0,0 +1,28 @@
+package github.com.jailcomfranssa.mapeamentobasico;
+
+import github.com.jailcomfranssa.EntityManagerTest;
+import github.com.jailcomfranssa.model.entities.Cliente;
+import github.com.jailcomfranssa.model.enums.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapeandoEnumeracoesTest extends EntityManagerTest {
+
+    @Test
+    public void testarEnum(){
+        Cliente cliente = new Cliente();
+        cliente.setId(4);
+        cliente.setNome("Zé Mineiro");
+        cliente.setSexo(SexoCliente.MASCULINO);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class,cliente.getId());
+        Assert.assertNotNull(clienteVerificacao);
+
+    }
+}
