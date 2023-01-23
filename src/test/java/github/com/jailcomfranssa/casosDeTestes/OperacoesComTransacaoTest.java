5cedab400ab92575deb02b03e40fc9ba02a81fea
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 14:55:23 -0300

    inserindo no banco

--- src/test/java/github/com/jailcomfranssa/casosDeTestes/OperacoesComTransacaoTest.java
+++ src/test/java/github/com/jailcomfranssa/casosDeTestes/OperacoesComTransacaoTest.java
@@ -2,10 +2,33 @@ package github.com.jailcomfranssa.casosDeTestes;
 
 import github.com.jailcomfranssa.EntityManagerTest;
 import github.com.jailcomfranssa.model.Produto;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
+    @Test
+    public void inserirOPrimeiroObjeto(){
+        Produto produto = new Produto();
+
+        produto.setId(2);
+        produto.setNome("Câmera Canon");
+        produto.setDescricao("A melhor definição para foto");
+        produto.setPreco(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(produto);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+    }
     @Test
     public void abrirEFecharATransacao(){
 //        Produto produto = new Produto();
