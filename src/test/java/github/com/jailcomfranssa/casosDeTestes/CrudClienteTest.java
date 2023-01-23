4589f6bfe1730e64502c00d417f8a046057b5009
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 17:08:58 -0300

    criando crud de test do Cliente

---
+++ src/test/java/github/com/jailcomfranssa/casosDeTestes/CrudClienteTest.java
@@ -0,0 +1,52 @@
+package github.com.jailcomfranssa.casosDeTestes;
+
+import github.com.jailcomfranssa.EntityManagerTest;
+import github.com.jailcomfranssa.model.Cliente;
+import org.junit.Test;
+
+public class CrudClienteTest extends EntityManagerTest {
+
+    @Test
+    public void inserindo(){
+        Cliente cliente = new Cliente();
+        cliente.setId(3);
+        cliente.setNome("Jose Pinto");
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(cliente);
+
+        entityManager.getTransaction().commit();
+    }
+
+    @Test
+    public void atulizando(){
+
+        Cliente cliente = new Cliente();
+        cliente.setId(3);
+        cliente.setNome("Jose Pinto Filho");
+
+        entityManager.getTransaction().begin();
+
+        entityManager.merge(cliente);
+
+        entityManager.getTransaction().commit();
+    }
+
+    @Test
+    public void consultando(){
+
+       Cliente cliente =  entityManager.find(Cliente.class,1);
+    }
+
+    @Test
+    public void deletando(){
+        Cliente cliente =  entityManager.find(Cliente.class,1);
+
+        entityManager.getTransaction().begin();
+
+        entityManager.remove(cliente);
+
+        entityManager.getTransaction().commit();
+    }
+}
