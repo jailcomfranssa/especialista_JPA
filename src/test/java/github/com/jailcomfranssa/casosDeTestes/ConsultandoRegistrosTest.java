d3e4bc3e9c1e66f4ae5451e95f990ce65576c2ea
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 13:33:27 -0300

    criando classe generica para tete

---
+++ src/test/java/github/com/jailcomfranssa/casosDeTestes/ConsultandoRegistrosTest.java
@@ -0,0 +1,29 @@
+package github.com.jailcomfranssa.casosDeTestes;
+
+import github.com.jailcomfranssa.EntityManagerTest;
+import github.com.jailcomfranssa.model.Produto;
+import org.junit.*;
+
+public class ConsultandoRegistrosTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPorIdentificador(){
+        Produto produto = entityManager.find(Produto.class,1);
+
+        // Por Referência não busca no Banco de dados.
+        //Produto produto1 = entityManager.getReference(Produto.class,1);
+
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle",produto.getNome());
+
+    }
+
+    @Test
+    public void atualizarAReferencia(){
+        Produto produto = entityManager.find(Produto.class,1);
+        produto.setNome("Microfone");
+
+        entityManager.refresh(produto);
+        Assert.assertEquals("Kindle",produto.getNome());
+    }
+}
