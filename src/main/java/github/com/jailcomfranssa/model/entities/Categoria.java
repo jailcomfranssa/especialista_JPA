2a656bc174134f34346d88a32dc78b2879983d77
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 20:28:03 -0300

    add anotações nas entidades

--- src/main/java/github/com/jailcomfranssa/model/entities/Categoria.java
+++ src/main/java/github/com/jailcomfranssa/model/entities/Categoria.java
@@ -4,13 +4,16 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "categoria")
 public class Categoria {
 
     @EqualsAndHashCode.Include
@@ -18,5 +21,7 @@ public class Categoria {
     private Integer id;
 
     private String nome;
+
+    @Column(name = "categoria_pai_id")
     private Integer categoriaPaiId;
 }
