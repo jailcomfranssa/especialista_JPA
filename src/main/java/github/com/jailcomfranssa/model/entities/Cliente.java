3bb0636d7fbde13b8963b397c96df75feedb216a
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-23 20:52:37 -0300

    mapenado enum para string

--- src/main/java/github/com/jailcomfranssa/model/entities/Cliente.java
+++ src/main/java/github/com/jailcomfranssa/model/entities/Cliente.java
@@ -5,9 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -20,6 +18,8 @@ public class Cliente {
     @Id
     private Integer id;
     private String nome;
+
+    @Enumerated(EnumType.STRING)
     private SexoCliente sexo;
 
 }
