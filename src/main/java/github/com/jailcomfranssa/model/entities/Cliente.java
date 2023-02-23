8515355cdf694583a02b4fad9009d1be67003d2c
jailcomfranssa <jail_son_0@hotmail.com>
2023-02-23 13:34:44 -0300

    Mapeando tabela secundária com @SecondaryTable

--- src/main/java/github/com/jailcomfranssa/model/entities/Cliente.java
+++ src/main/java/github/com/jailcomfranssa/model/entities/Cliente.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"))
 @Entity
 @Table(name = "cliente")
 public class Cliente {
@@ -23,15 +25,19 @@ public class Cliente {
     private Integer id;
     private String nome;
 
+    @Column(table = "cliente_detalhe")
+    @Enumerated(EnumType.STRING)
+    private SexoCliente sexo;
+
+    @Column(name = "data_nascimento", table = "cliente_detalhe")
+    private LocalDate dataNascimento;
+
     @ElementCollection
     @CollectionTable(name = "clienye_contao",
     joinColumns = @JoinColumn(name = "cliente_id"))
     @MapKeyColumn(name = "tipo")
     @Column(name = "descrição")
     private Map<String, String> contatos;
-
-    @Enumerated(EnumType.STRING)
-    private SexoCliente sexo;
 
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
---
+++ src/test/java/github/com/jailcomfranssa/mapeamentoavancado/SecondaryTableTest.java
@@ -0,0 +1,29 @@
+package github.com.jailcomfranssa.mapeamentoavancado;
+
+import github.com.jailcomfranssa.EntityManagerTest;
+import github.com.jailcomfranssa.model.entities.Cliente;
+import github.com.jailcomfranssa.model.enums.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class SecondaryTableTest extends EntityManagerTest {
+
+    @Test
+    public void salvarCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Carlos Finotti");
+        cliente.setSexo(SexoCliente.MASCULINO);
+        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVerificacao.getSexo());
+    }
+}
